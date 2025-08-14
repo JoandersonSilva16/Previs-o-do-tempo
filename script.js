fa6a80826f325f295083ab8b44e171f48de01b1e
JoandersonSilva16 <jsa35@aluno.ifal.edu.br>
2025-08-13 22:57:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const inputCidade = document.getElementById("city-input");
+const botaoBuscar = document.getElementById("search-button");
+const resultadoClima = document.getElementById("weather-result");
+const mensagemErro = document.getElementById("error-message");
+const nomeCidadePais = document.getElementById("location");
+const horarioLocal = document.getElementById("local-time");
+const imagemClima = document.getElementById("weather-icon");
+const temperatura = document.getElementById("temperature");
+const condicao = document.getElementById("condition");
+const sensacao = document.getElementById("feels-like");
+const umidade = document.getElementById("humidity");
+const vento = document.getElementById("wind");
+const pressao = document.getElementById("pressure");
+const visibilidade = document.getElementById("visibility");
+const indiceUV = document.getElementById("uv");
 
+<script src = "script.js"></script>
